fbc0b2dec4745f40a522899dc48a8e1c03c9de5a
Johan Östling <jostling@baffinbaynetworks.com>
2019-12-28 17:47:26 +0100

    master hello world

---
+++ testProject/Controllers/WeatherForecastController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace testProject.Controllers
+{
+    // [ApiController]
+    // [Route("[controller]")]
+    [Route("api/v1")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        // [HttpGet]
+        // public IEnumerable<WeatherForecast> Get()
+        // {
+
+        //     System.Console.WriteLine("@@@@@@@@@@@");
+    
+        //     var rng = new Random();
+        //     return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //     {
+        //         Date = DateTime.Now.AddDays(index),
+        //         TemperatureC = rng.Next(-20, 55),
+        //         Summary = Summaries[rng.Next(Summaries.Length)]
+        //     })
+        //     .ToArray();
+        // }
+
+
+        [HttpGet("hello-world")]
+        public IActionResult GetHelloWorld()
+        {
+
+            return new OkObjectResult("hello world !!!");
+        }
+    }
+}
